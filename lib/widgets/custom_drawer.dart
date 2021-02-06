79f30d2aa43cb3a326b3c4631b71d241c1a7614d
felipeemidio <lipe.emidio@gmail.com>
2021-02-06 19:35:17 -0300

    Drawer's Items

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/tiles/drawer_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
   Widget _buildDrawerBack() => Container(
@@ -38,9 +39,41 @@ class CustomDrawer extends StatelessWidget {
                       ),
                     ),
                   ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("Olá",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        GestureDetector(
+                          child: Text("Entre ou cadastre-se >",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          onTap: () {
+
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
-            )
+            ),
+            Divider(),
+            DrawerTile(Icons.home, "Início"),
+            DrawerTile(Icons.list, "Produtos"),
+            DrawerTile(Icons.location_on, "Lojas"),
+            DrawerTile(Icons.playlist_add_check, "Meus Pedidos"),
           ],
         ),
       ],),
