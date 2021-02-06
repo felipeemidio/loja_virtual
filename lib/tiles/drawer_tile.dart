8a753807ad6e0b5ce365aad9f776d3c818bb94c6
felipeemidio <lipe.emidio@gmail.com>
2021-02-06 19:44:18 -0300

    Navigation by Drawer

--- lib/tiles/drawer_tile.dart
+++ lib/tiles/drawer_tile.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 class DrawerTile extends StatelessWidget {
   final IconData icon;
   final String text;
+  final PageController pageController;
+  final int page;
 
-  DrawerTile(this.icon, this.text);
+  DrawerTile(this.icon, this.text, this.pageController, this.page);
 
   @override
   Widget build(BuildContext context) {
@@ -12,19 +14,24 @@ class DrawerTile extends StatelessWidget {
       color: Colors.transparent,
       child: InkWell(
         onTap: () {
-          
+          Navigator.of(context).pop();
+          pageController.jumpToPage(page);
         },
         child: Container(
           height: 60,
           child: Row(
             children: [
-              Icon(icon, size: 32, color: Colors.black,),
+              Icon(
+                icon,
+                size: 32,
+                color: pageController.page.round() == page ? Theme.of(context).primaryColor : Colors.grey[700],
+              ),
               SizedBox(width: 32),
               Text(
                 text,
                 style: TextStyle(
                   fontSize: 16,
-                  color: Colors.black,
+                  color: pageController.page.round() == page ? Theme.of(context).primaryColor : Colors.grey[700],
                 ),
               ),
 
