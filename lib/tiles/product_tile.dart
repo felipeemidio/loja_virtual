4bb0b937322084552a389ea664a5233c4ea73f9f
felipeemidio <lipe.emidio@gmail.com>
2021-02-11 23:00:32 -0300

    Product list tile

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
+import 'package:loja_virtual/screens/product_screen.dart';
 
 class ProductTile extends StatelessWidget {
   final String type;
@@ -10,6 +11,11 @@ class ProductTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(builder: (context) => ProductScreen(product))
+        );
+      },
       child: Card(
         child: type == "grid"
             ? Column(
@@ -24,28 +30,63 @@ class ProductTile extends StatelessWidget {
                     ),
                   ),
                   Expanded(
-                      child: Container(
-                    padding: EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          product.title,
-                          style: TextStyle(fontWeight: FontWeight.w500),
-                        ),
-                        Text(
-                          'R\$ ${product.price.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 17,
-                            fontWeight: FontWeight.bold,
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
                           ),
-                        ),
-                      ],
+                          Text(
+                            'R\$ ${product.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  )),
+                  ),
                 ],
               )
-            : Row(),
+            : Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Image.network(
+                      product.images[0],
+                      fit: BoxFit.cover,
+                      height: 250,
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            product.title,
+                            style: TextStyle(fontWeight: FontWeight.w500),
+                          ),
+                          Text(
+                            'R\$ ${product.price.toStringAsFixed(2)}',
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 17,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
       ),
     );
   }
