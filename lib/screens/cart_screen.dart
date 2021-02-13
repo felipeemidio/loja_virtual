7707faf5a8c3f969c48af938bd16e96f2ad72776
felipeemidio <lipe.emidio@gmail.com>
2021-02-13 18:12:04 -0300

    Product card tile

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/screens/login_screen.dart';
+import 'package:loja_virtual/tiles/card_tile.dart';
 import 'package:scoped_model/scoped_model.dart';
 
 class CartScreen extends StatelessWidget {
@@ -80,7 +81,15 @@ class CartScreen extends StatelessWidget {
             );
           }
           else {
-            return Container();
+            return ListView(
+              children: [
+                Column(
+                  children: model.products.map((product) {
+                    return CardTile(product);
+                  }).toList(),
+                ),
+              ],
+            );
           }
         },
       ),
