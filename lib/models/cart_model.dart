c1c8fe2223c8a7913deefd5a21873dceeb468bb9
felipeemidio <lipe.emidio@gmail.com>
2021-02-13 17:35:06 -0300

    Add a body to CartScreen

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -7,6 +7,7 @@ import 'package:scoped_model/scoped_model.dart';
 class CartModel extends Model {
   UserModel user;
   List<CartProduct> products = [];
+  bool isLoading = false;
 
   CartModel(this.user);
 
